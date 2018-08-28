270074af78e847e4541d90e456e0c23428470dd1
cong_yu <cong.yu@nuance.com>
2018-08-28 17:18:27 -0400

    commit changes before leaving

--- lintcode-practice/src/main/java/intArray/FirstMissingPositive189.java
+++ lintcode-practice/src/main/java/intArray/FirstMissingPositive189.java
@@ -1,5 +1,7 @@
 package intArray;
 
+import java.util.Arrays;
+
 public class FirstMissingPositive189 {
 
 	 /**
@@ -9,12 +11,28 @@ public class FirstMissingPositive189 {
      * @param A: An array of integers
      * @return: An integer
      */
+	
+	// This approach requries modifying the original array
     public int firstMissingPositive(int[] A) {
+    	if(A == null || A.length == 0)
+    	{
+    		return 0;
+    	}
+    	int size = A.length;
+    	for(int i = 0 ; i < size; i++)
+    	{
+    		
+    	}
     	return 0;
     }
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int [] sample1 = new int [] {1, 2, 0};
+		int [] sample2 = new int [] {3, 4, -1, 1};
+		FirstMissingPositive189 test = new FirstMissingPositive189();
+		System.out.println(test.firstMissingPositive(sample1));
+		System.out.println(test.firstMissingPositive(sample2));
 
 	}
 
