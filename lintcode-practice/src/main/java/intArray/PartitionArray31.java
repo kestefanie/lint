d2b29ccf56a5cdfd49f47235f41af73f34ec4412
cong_yu <cong.yu@nuance.com>
2018-08-28 23:18:51 -0400

    commit all cases for array

--- lintcode-practice/src/main/java/intArray/PartitionArray31.java
+++ lintcode-practice/src/main/java/intArray/PartitionArray31.java
@@ -2,6 +2,16 @@ package intArray;
 
 public class PartitionArray31 {
 
+	 /**
+     * @param nums: The integer array you should partition
+     * @param k: An integer
+     * @return: The index after partition
+     */
+    public int partitionArray(int[] nums, int k) {
+        // write your code here
+    		return 0;
+    }
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
--- lintcode-practice/src/main/java/intArray/ThreeSumClosest59.java
+++ lintcode-practice/src/main/java/intArray/ThreeSumClosest59.java
@@ -2,6 +2,23 @@ package intArray;
 
 public class ThreeSumClosest59 {
 
+	/**
+	 * 
+	 * similiar to 2sum, using two pointer
+	 * 
+	 * but this time, make one fix, and another two pointers...
+	 * 
+	 * @param numbers:
+	 *            Give an array numbers of n integer
+	 * @param target:
+	 *            An integer
+	 * @return: return the sum of the three integers, the sum closest target.
+	 */
+	public int threeSumClosest(int[] numbers, int target) {
+		// write your code here
+		return 0;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
