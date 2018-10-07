5212ea67e83edc3b67d7c69545de57e86cbce2e3
cong_yu <cong.yu@nuance.com>
2018-10-07 16:46:03 -0400

    commit the change

--- lintcode-practice/src/main/java/binarySearch/FindMinInRotatedSortedArray159.java
+++ lintcode-practice/src/main/java/binarySearch/FindMinInRotatedSortedArray159.java
@@ -2,9 +2,51 @@ package binarySearch;
 
 public class FindMinInRotatedSortedArray159 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	/**
+	 * @param nums:
+	 *            a rotated sorted array
+	 * @return: the minimum number in the array
+	 * 
+	 * process: 
+	 * 
+	 */
+	public int findMin(int[] nums) {
+		int result = -1;
+		if (nums != null && nums.length != 0) {
+			int start = 0;
+			int end = nums.length - 1;
+			// check if the array is rotated
+			if(nums[end] >= nums[start])
+			{
+				return nums[start];
+			}
+			while (end >= start) {
+				int middle = (start + end) / 2;
+				int value = nums[middle];
+				//System.out.println("start=" + start + " middle=" + middle + " end=" + end);
+				if (value >= nums[start]) {
+					start = middle + 1;
+					// this is important!!!!
+					if(nums[start] < value)
+					{
+						return nums[start];
+					}
+				} else {
+					if(middle !=0 && value < nums[middle-1])
+					{
+						return value;
+					}
+					end = middle - 1;
+				}
+			}
+		}
+		return result;
+	}
 
+	public static void main(String[] args) {
+		FindMinInRotatedSortedArray159 test = new FindMinInRotatedSortedArray159();
+		int[] sample = new int[]{4, 5, 6, 7, 0, 1, 2};
+		System.out.println(test.findMin(sample) == 0);
 	}
 
 }
